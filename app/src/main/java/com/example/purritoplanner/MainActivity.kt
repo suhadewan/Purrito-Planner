25aaac15f1dd629f30170ad4a3d48dde40d14c9b
shaylacarter <shaylac@vt.edu>
2022-11-17 00:55:51 -0500

    Added basic navigation using the bottom bar

--- app/src/main/java/com/example/purritoplanner/MainActivity.kt
+++ app/src/main/java/com/example/purritoplanner/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.purritoplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onNavigationClick(view: View) {
+        if (view is ImageButton) {
+            val navController = findNavController(R.id.main_screen_fragment_holder)
+            navController.popBackStack(R.id.homeScreenFragment, false)
+            when (view.id) {
+                R.id.homeButton -> {
+                    navController.navigate(R.id.homeScreenFragment)
+                }
+                R.id.planButton -> {
+                    navController.navigate(R.id.calendarFragment)
+                }
+                R.id.mealButton -> {
+                    //We haven't implemented this page yet!
+                }
+                R.id.shopButton -> {
+                    navController.navigate(R.id.shoppingListFragment)
+                }
+            }
+        }
     }
 }
