8c682a8ba247e93114d49313cf78e71424c61321
shaylacarter <shaylac@vt.edu>
2022-11-17 00:06:16 -0500

    Set up for adding fragment code

--- app/src/main/java/com/example/purritoplanner/HomeFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.purritoplanner;
-
-import androidx.fragment.app.Fragment;
-
-public class HomeFragment extends Fragment {
-}
---
+++ app/src/main/java/com/example/purritoplanner/ViewRecipeFragment.kt
@@ -0,0 +1,6 @@
+package com.example.purritoplanner
+
+import androidx.fragment.app.Fragment
+
+class ViewRecipeFragment : Fragment() {
+}
